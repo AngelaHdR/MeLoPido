b681888159b0360f56349542e334c73d48692119
Angela Hermosilla del Rio <angelahermosillad@gmail.com>
2025-03-23 19:05:17 +0100

    fix(model): add username and password getter and setter

--- domain/src/main/java/com/fpmislata/MeLoPido/domain/model/User.java
+++ domain/src/main/java/com/fpmislata/MeLoPido/domain/model/User.java
@@ -101,4 +101,20 @@ public class User {
     public void setSurname2(String surname2) {
         this.surname2 = surname2;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
