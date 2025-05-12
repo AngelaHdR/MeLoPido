3230f6c72e79dd45a7c5de31493f11f7a67c1624
Angela Hermosilla del Rio <angelahermosillad@gmail.com>
2025-05-12 19:00:13 +0200

    modify repository implement to accept dao

--- persistence/src/main/java/com/fpmislata/MeLoPido/persistence/repository/impl/letter/LetterRepositoryImpl.java
+++ persistence/src/main/java/com/fpmislata/MeLoPido/persistence/repository/impl/letter/LetterRepositoryImpl.java
@@ -4,57 +4,48 @@ import com.fpmislata.MeLoPido.domain.model.Letter;
 import com.fpmislata.MeLoPido.persistence.dao.LetterDao;
 import com.fpmislata.MeLoPido.persistence.dao.jpa.entity.LetterEntity;
 import com.fpmislata.MeLoPido.persistence.dao.jpa.mapper.LetterEntityMapper;
-import com.fpmislata.MeLoPido.persistence.dao.jpa.repository.LetterJpaRepository;
 import com.fpmislata.MeLoPido.util.pagination.ListWithCount;
 import com.fpmislata.MeLoPido.domain.repository.LetterRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public class LetterRepositoryImpl implements LetterRepository {
-    //private LetterJpaRepository letterJpaRepository;
-    private LetterDao letterJpaRepository;
+    private LetterDao letterDao;
 
-    public LetterRepositoryImpl(LetterDao letterJpaRepository) {
-        this.letterJpaRepository = letterJpaRepository;
+    public LetterRepositoryImpl(LetterDao letterDao) {
+        this.letterDao = letterDao;
     }
 
-    //TODO: como se haria el UUID
     @Override
     public ListWithCount<Letter> findAll(int page, int pageSize) {
-        ListWithCount<LetterEntity> pages = letterJpaRepository.findAll(page, pageSize);
-        return new ListWithCount<>(pages.getList().stream().map(LetterEntityMapper::toLetter).toList(), pages.getCount());
+        ListWithCount<LetterEntity> list = letterDao.findAll(page, pageSize);
+        return new ListWithCount<>(list.getList().stream().map(LetterEntityMapper::toLetter).toList(), list.getCount());
     }
 
     @Override
     public Optional<Letter> findById(String id) {
-        return letterJpaRepository.findById(id).map(LetterEntityMapper::toLetter);
+        return letterDao.findById(id).map(LetterEntityMapper::toLetter);
     }
 
     @Override
     public void delete(String id) {
-        //letterJpaRepository.deleteById(id);
+        letterDao.delete(id);
     }
-
-
 
     @Override
     public void save(Letter letter) {
-        letterJpaRepository.save(LetterEntityMapper.toLetterEntity(letter));
+        letterDao.save(LetterEntityMapper.toLetterEntity(letter));
     }
 
     @Override
     public ListWithCount<Letter> findAllByUser(int page, int pageSize, String idUser) {
-        ListWithCount<LetterEntity> pages = letterJpaRepository.findAllByUser(page,pageSize, idUser);
-        return new ListWithCount<>(pages.getList().stream().map(LetterEntityMapper::toLetter).toList(), pages.getCount());
+        ListWithCount<LetterEntity> list = letterDao.findAllByUser(page,pageSize, idUser);
+        return new ListWithCount<>(list.getList().stream().map(LetterEntityMapper::toLetter).toList(), list.getCount());
     }
 
     @Override
     public ListWithCount<Letter> findAllByGroup(int page, int pageSize, String idGroup) {
-        ListWithCount<LetterEntity> pages = letterJpaRepository.findAllByGroup(page, pageSize, idGroup);
-        return new ListWithCount<>(pages.getList().stream().map(LetterEntityMapper::toLetter).toList(), pages.getCount());
+        ListWithCount<LetterEntity> list = letterDao.findAllByGroup(page, pageSize, idGroup);
+        return new ListWithCount<>(list.getList().stream().map(LetterEntityMapper::toLetter).toList(), list.getCount());
     }
 }
